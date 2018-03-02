79465fef01cff5690f00a3ec447895dbe3d206bb
Dakinola892 <dakinola892@gmail.com>
2018-03-02 13:34:05 +0000

    Fixed so function simply returns int instead of printing with 'puts'

--- lib/exchangerate.rb
+++ lib/exchangerate.rb
@@ -1,5 +1,28 @@
-require "exchangerate/version"
+require "exchangerate/rate_finder"
+require "date"
 
-module Exchangerate
-  # Your code goes here...
+#TODO: add better exception handling, more useful messages, etc.
+
+class ExchangeRate
+  def self.at(date = Date.today, base_currency, counter_currency)
+    if base_currency != counter_currency
+      date_string = date.to_s
+
+      base_currency_rate = RateFinder.find_rate_for_currency(date, base_currency)
+      counter_currency_rate = RateFinder.find_rate_for_currency(date, counter_currency)
+
+      if base_currency_rate.to_s.empty? or counter_currency_rate.to_s.empty?
+        return puts "Invalid request"
+      end
+
+      result_rate = counter_currency_rate / base_currency_rate
+
+      #puts "%.4f" % result_rate
+    else
+      return 1.000
+      #puts "1.0000"
+    end
+  end
 end
+
+#ExchangeRate.at(Date.today.prev_day, 'AAA', 'USD')
